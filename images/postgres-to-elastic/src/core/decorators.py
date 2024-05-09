b5c0732e2b9ce98cd52b73d540a798aaa1b1ea91
MironBerch <mironberchetov@gmail.com>
2024-05-09 16:57:14 +0300

    Create backoff decorator and logger config for `postgres-to-elastic`

---
+++ images/postgres-to-elastic/src/core/decorators.py
@@ -0,0 +1,27 @@
+from time import sleep
+from typing import Any, Callable
+
+from core.logger import logger
+
+
+def backoff(exceptions: tuple[Exception], sleep_time: float = 1.0) -> Callable:
+    """
+    Функция для повторного выполнения функции через некоторое время, если возникла ошибка.
+
+    Args:
+        exceptions: ошибки, которые нужно обработать
+        sleep_time: время повтора
+    """
+    def decorator(func) -> Callable:
+        def wrapper(*args, **kwargs) -> Any:
+            while True:
+                try:
+                    connection = func(*args, **kwargs)
+                except exceptions as message:
+                    logger.error(f'Нет соединения: {message}')
+                    logger.error(f'Повторное подключение через {sleep_time}с')
+                    sleep(sleep_time)
+                else:
+                    return connection
+        return wrapper
+    return decorator
