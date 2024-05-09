872d2f77a3a64b1195da57748dd852347aae51fd
MironBerch <mironberchetov@gmail.com>
2024-05-09 13:39:35 +0300

    Create pydantic models for `postgres-to-elastic`
    Create pydantic models for `postgres-to-elastic`.
    Update `postgres-to-elastic` requirements.

---
+++ images/postgres-to-elastic/src/models/movie.py
@@ -0,0 +1,32 @@
+from typing import Any, ClassVar
+
+from models.base import UUIDMixin
+from models.person import Person
+
+
+class Movie(UUIDMixin):
+    """Класс для валидации данных `filmwork`."""
+
+    imdb_rating: float
+    genre: list[str]
+    title: str
+    description: str
+    director: list[str]
+    actors_names: list[str]
+    writers_names: list[str]
+    actors: list[Person]
+    writers: list[Person]
+    _index: ClassVar[str] = 'movies'
+
+    @classmethod
+    def properties(cls, **kwargs) -> dict[str, Any]:
+        """Возвращает схему модели фильма с её характеристиками."""
+        properties: dict[str, Any] = {}
+        for field, value in cls.model_json_schema(**kwargs)['properties'].items():
+            if value['type'] == 'string':
+                properties[field] = ''
+            if value['type'] == 'array':
+                properties[field] = []
+            if value['type'] == 'number':
+                properties[field] = 0
+        return properties
